199eda9c42a9af60abfe277e93a679a98249495d
Mussie <mussieayub@gmail.com>
2023-11-20 00:09:15 -0500

    "second commit"

---
+++ springSecurity/src/main/java/com/mose/springSecurity/user/Role.java
@@ -0,0 +1,6 @@
+package com.mose.springSecurity.user;
+
+public enum Role {
+    ADMIN,
+    USER
+}
---
+++ springSecurity/src/main/java/com/mose/springSecurity/user/User.java
@@ -0,0 +1,58 @@
+package com.mose.springSecurity.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name="_user")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
