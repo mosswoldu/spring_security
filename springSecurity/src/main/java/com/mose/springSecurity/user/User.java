4307edc589146cbed413e971e00bb32709d6416b
Mussie <mussieayub@gmail.com>
2023-11-20 12:26:10 -0500

    "second commit"

--- springSecurity/src/main/java/com/mose/springSecurity/user/User.java
+++ springSecurity/src/main/java/com/mose/springSecurity/user/User.java
@@ -6,9 +6,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -24,35 +26,69 @@ public class User implements UserDetails {
     private String surname;
     private String email;
     private String password;
-    private Role role;
+   @Enumerated(EnumType.STRING)
+   private Role role;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return List.of(new SimpleGrantedAuthority(role.name()));
     }
+    /*public Set<GrantedAuthority> getAuthorities() {
+        return new HashSet<>(Arrays.asList(new SimpleGrantedAuthority(role.name())));
+    }
+
+    public Iterator<GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority(role.name())).iterator();
+    }
+
+    public Tuple<GrantedAuthority> getAuthorities() {
+        return new Tuple<>(new SimpleGrantedAuthority(role.name()));
+    }
+
+    public Optional<GrantedAuthority> getAuthorities() {
+        return Optional.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    public Dict<String, GrantedAuthority> getAuthorities() { //to store the collection in a map
+        return new HashMap<>() {{
+            put("role", new SimpleGrantedAuthority(role.name()));
+        }};
+    }
+
+    public Callable<GrantedAuthority> getAuthorities() {
+        return () -> new SimpleGrantedAuthority(role.name());
+    }
+
+    public Any getAuthorities() {
+        return new SimpleGrantedAuthority(role.name());
+    }*/
 
     @Override
     public String getUsername() {
-        return null;
+        return email;
+    }
+    @Override
+    public String getPassword() {
+        return password;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
